3650a7b2ff7790f299710da0d7e28348c495f733
Vadim Shaidurov <shaidurovvadik@yandex.ru>
2024-02-21 22:52:24 +0300

    Commit

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from github import Github
-# import magic
 import csv
+
+SKIP_FILES = ['__init__.py']
+
+lang_ext = {
+        'Python': '.py',
+        'Java': '.java',
+        'JavaScript': '.js',
+        'C': '.c',
+        'C++': '.cpp',
+        'C#': '.cs',
+        'TypeScript': '.ts',
+        'PHP': '.php',
+        'Swift': '.swift',
+        'Go': '.go'
+    }
+
+languages = 'Python'
 
 
 def process_repo_contents(contents, repo, csv_writer):
     for content in contents:
         if content.type == "dir":
             process_repo_contents(repo.get_contents(content.path), repo, csv_writer)
-        elif content.type == "file" and content.name.endswith('.py'):
+        elif (content.type == "file"
+              and content.name.endswith(lang_ext[languages])
+              and content.name not in SKIP_FILES):
             file_content = repo.get_contents(content.path).decoded_content
             code_lines = len(file_content.splitlines())
-            if content.name != "__init__.py":
-                csv_writer.writerow([repo.name, content.path, content.name, code_lines])
+            csv_writer.writerow([repo.name, content.path, content.name, code_lines])
             # print(f"Repository:{repo.name}, Path:{content.path}, File Name:{content.name}, Code Length:{code_lines}")
 
 
@@ -27,14 +44,14 @@ def get_repo_file_info(repo, csv_writer):
 
 
 def main():
-    access_token = "Your Token"
+    access_token = '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'
     g = Github(access_token)
     user = g.get_user()
 
     # Going through all the user's repositories
-    with open("github.csv", "w", newline="") as csvfile:
+    with open("progress/github.csv", "w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["Repository", "Path", "File Name", "Code Length"])
+        csv_writer.writerow(["Repo", "Path", "File", "Lines of Code"])
 
         for repo in user.get_repos():
             get_repo_file_info(repo, csv_writer)
--- temp/GitHub-Statistics/README.md
+++
@@ -1,4 +0,0 @@
-# GitHub-Statistics
-My goal was to find out the average length of my (.py) file. 
-
-I didn't include (.ipynb) files in my research, because I'm convinced that these files should only contain graphical information (not the main working code).
